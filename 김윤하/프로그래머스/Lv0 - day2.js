4cfb3f6bf20a1d25aa9472695a83b4408969a210
yulloe <beupthere@gmail.com>
2023-08-24 17:05:00 +0900

    [PGS] Lv0.홀짝에 따라 다른 값 반환하기

--- 김윤하/프로그래머스/Lv0 - day2.js
+++ 김윤하/프로그래머스/Lv0 - day2.js
@@ -5,3 +5,17 @@ function n의_배수(num, n) {
 function 공배수(number, n, m) {
   return number % n === 0 && number % m === 0 ? 1 : 0;
 }
+
+function 홀짝에_따라_다른_값_반환하기(n) {
+  var answer = 0;
+  if (n % 2) {
+    for (let i = 1; i < n + 1; i += 2) {
+      answer += i;
+    }
+  } else {
+    for (let i = 2; i < n + 1; i += 2) {
+      answer += i ** 2;
+    }
+  }
+  return answer;
+}
