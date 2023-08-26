874afb2deff352acf0ddcba3083911e29b119783
yulloe <beupthere@gmail.com>
2023-08-26 17:09:11 +0900

    [PGS] Lv0.수 조작하기 2

--- 김윤하/프로그래머스/Lv0 - day2.js
+++ 김윤하/프로그래머스/Lv0 - day2.js
@@ -146,3 +146,24 @@ function 수_조작하기_1(n, control) {
   }
   return n;
 }
+
+function 수_조작하기_2(numLog) {
+  var answer = "";
+  for (let i = 0; i < numLog.length - 1; i++) {
+    switch (numLog[i + 1] - numLog[i]) {
+      case 1:
+        answer += "w";
+        break;
+      case -1:
+        answer += "s";
+        break;
+      case 10:
+        answer += "d";
+        break;
+      case -10:
+        answer += "a";
+        break;
+    }
+  }
+  return answer;
+}
