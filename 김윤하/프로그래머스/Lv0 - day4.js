18b99055f4c1f035be07567ff590c2350132256a
yulloe <beupthere@gmail.com>
2023-08-30 18:23:19 +0900

    [PGS] Lv0.카운트 다운

--- 김윤하/프로그래머스/Lv0 - day4.js
+++ 김윤하/프로그래머스/Lv0 - day4.js
@@ -74,3 +74,11 @@ function 글자_지우기(my_string, indices) {
   }
   return strArr.join("");
 }
+
+function 카운트_다운(start_num, end_num) {
+  var answer = [];
+  for (let i = start_num; i >= end_num; i--) {
+    answer.push(i);
+  }
+  return answer;
+}
