526c3f199c27d0282fd36736c7c595ae9bbfea68
yulloe <beupthere@gmail.com>
2023-08-30 14:41:54 +0900

    [PGS] Lv0.접두사인지 확인하기

--- 김윤하/프로그래머스/Lv0 - day4.js
+++ 김윤하/프로그래머스/Lv0 - day4.js
@@ -2,3 +2,8 @@ function 문자열의_앞의_n글자(my_string, n) {
   var answer = my_string.slice(0, n);
   return answer;
 }
+
+function 접두사인지_확인하기(my_string, is_prefix) {
+  var answer = Number(my_string.startsWith(is_prefix));
+  return answer;
+}
