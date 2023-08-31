d7d9b54270640dcf476ffbefd833d5244a40600c
yulloe <beupthere@gmail.com>
2023-08-31 17:57:08 +0900

    [PGS] Lv0.순서 바꾸기

--- 김윤하/프로그래머스/Lv0 - day5.js
+++ 김윤하/프로그래머스/Lv0 - day5.js
@@ -2,3 +2,10 @@ function n_번째_원소부터(num_list, n) {
   var answer = num_list.slice(n - 1);
   return answer;
 }
+
+function 순서_바꾸기(num_list, n) {
+  var answer = [];
+  var after_n = num_list.slice(n);
+  var before_n = num_list.slice(0, n);
+  return after_n.concat(...before_n);
+}
