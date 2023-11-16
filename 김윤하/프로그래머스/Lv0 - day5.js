fdbf1a41a733d348a81b612787e0d6eb6cae2e6d
yulloe <beupthere@gmail.com>
2023-11-16 19:52:40 +0900

    [PGS] Lv0.왼쪽 오른쪽

--- 김윤하/프로그래머스/Lv0 - day5.js
+++ 김윤하/프로그래머스/Lv0 - day5.js
@@ -9,3 +9,14 @@ function 순서_바꾸기(num_list, n) {
   var before_n = num_list.slice(0, n);
   return after_n.concat(...before_n);
 }
+
+function 왼쪽_오른쪽(str_list) {
+  var idx = str_list.findIndex((el) => el === "l" || el === "r");
+  if (idx === -1) {
+    return [];
+  }
+  const isL = str_list[idx] === "l";
+  const start = isL ? 0 : idx + 1;
+  const end = isL ? idx : undefined;
+  return str_list.slice(start, end);
+}
