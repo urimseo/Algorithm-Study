b5a9adf8ab59852f25bc27e1ef4c09c2d4c94313
yulloe <beupthere@gmail.com>
2024-01-07 18:19:51 +0900

    [PGS] Lv0.길이에 따른 연산

--- 김윤하/프로그래머스/Lv0 - day6.js
+++ 김윤하/프로그래머스/Lv0 - day6.js
@@ -8,3 +8,16 @@ function 일1로_만들기(num_list) {
   });
   return cnt;
 }
+
+function 길이에_따른_연산(num_list) {
+  var answer = 1;
+  if (num_list.length >= 11) {
+    num_list.forEach((num) => (answer += num));
+    answer--;
+  } else {
+    num_list.forEach((num) => (answer *= num));
+  }
+  return answer;
+  // reduce로 풀기
+  // return num_list.reduce((a,v)=> num_list.length > 10 ? a+v : a*v )
+}
