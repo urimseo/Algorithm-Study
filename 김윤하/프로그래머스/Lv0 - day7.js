48a54a83b2700d99192e02b2f8808258dc233b21
yulloe <beupthere@gmail.com>
2024-01-10 13:53:14 +0900

    [PGS] Lv0.세 개의 구분자

--- 김윤하/프로그래머스/Lv0 - day7.js
+++ 김윤하/프로그래머스/Lv0 - day7.js
@@ -22,3 +22,8 @@ function 문자열_바꿔서_찾기(myString, pat) {
 function rny_string(rny_string) {
   return rny_string.replaceAll('m', 'rn');
 }
+
+function 세_개의_구분자(myStr) {
+  const result = myStr.split(/[a|b|c]/g).filter((a) => a);
+  return result.length ? result : ['EMPTY'];
+}
