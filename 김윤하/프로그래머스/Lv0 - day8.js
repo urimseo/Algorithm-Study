7dfe20c2d7d70a302b8dc251b7bbf578b125029d
yulloe <beupthere@gmail.com>
2024-01-16 11:20:23 +0900

    [PGS] Lv0.이차원 배열 대각선 순회하기

--- 김윤하/프로그래머스/Lv0 - day8.js
+++ 김윤하/프로그래머스/Lv0 - day8.js
@@ -145,3 +145,15 @@ function 정사각형으로_만들기(arr) {
   }
   return answer;
 }
+
+function 이차원_배열_대각선_순회하기(board, k) {
+  var answer = 0;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (i + j <= k) {
+        answer += board[i][j];
+      }
+    }
+  }
+  return answer;
+}
