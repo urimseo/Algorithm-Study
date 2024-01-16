6dddd5c3507b911db3e59e327c84b9e8dc1dd060
yulloe <beupthere@gmail.com>
2024-01-16 11:15:02 +0900

    [PGS] Lv0.주사위 게임 1

--- 김윤하/프로그래머스/Lv0 - day8.js
+++ 김윤하/프로그래머스/Lv0 - day8.js
@@ -1,3 +1,13 @@
 function 정수_찾기(num_list, n) {
   return num_list.includes(n) ? 1 : 0;
 }
+
+function 주사위_게임_1(a, b) {
+  const answer =
+    a % 2 && b % 2
+      ? a ** 2 + b ** 2
+      : a % 2 || b % 2
+      ? 2 * (a + b)
+      : Math.abs(a - b);
+  return answer;
+}
