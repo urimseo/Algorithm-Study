db920d126798bd3024cc82e381a51bcd92b0be06
yulloe <beupthere@gmail.com>
2024-01-16 11:20:57 +0900

    [PGS] Lv0.옹알이 (1)

--- 김윤하/프로그래머스/Lv0 - day8.js
+++ 김윤하/프로그래머스/Lv0 - day8.js
@@ -157,3 +157,17 @@ function 이차원_배열_대각선_순회하기(board, k) {
   }
   return answer;
 }
+
+function 옹알이_1(babbling) {
+  const one = ['aya', 'ye', 'woo', 'ma'];
+  let count = 0;
+  for (let i = 0; i < babbling.length; i++) {
+    for (let j = 0; j < one.length; j++) {
+      babbling[i] = babbling[i].replace(one[j], '-');
+    }
+  }
+  for (let k = 0; k < babbling.length; k++) {
+    if (babbling[k].split('-').join('') === '') count++;
+  }
+  return count;
+}
